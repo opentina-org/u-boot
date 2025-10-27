a008b85594deedd44a0eab970542d0ece412afa8
Yixun Lan <dlan@gentoo.org>
2025-10-27 21:28:55 +0000

    ddr: parse dram size from vendor boot0 header
    Vendor boot0 will retrieve dram size and pass it to
    next bootloader stage via the boot0 header.
    
    Reuse this scheme to get dram size for mainline u-boot.
    
    Signed-off-by: Yixun Lan <dlan@gentoo.org>

--- arch/arm/mach-sunxi/dram_sun60i_a733.c
+++ arch/arm/mach-sunxi/dram_sun60i_a733.c
@@ -1,6 +1,15 @@
+#include <init.h>
+#include <spl.h>
+#include <asm/io.h>
 #include <linux/sizes.h>
+
+struct sunxi_a733_dram {
+	unsigned int resv[318];
+	unsigned int size;
+};
 
 unsigned long sunxi_dram_init(void)
 {
-	return SZ_2G;
+	struct sunxi_a733_dram *d = (struct sunxi_a733_dram *)(CONFIG_SPL_TEXT_BASE & 0xFFFF0000);
+	return (unsigned long)d->size * SZ_1M;
 }
